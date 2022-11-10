39c0edf70d87d14e8c44207639efb460f09a405e
Ignacio <ptcnachlord996@outlook.com>
2022-11-10 10:08:51 -0300

    Sign In Test

--- app/src/main/java/com/notableFactory/marvelapp/MainActivity.kt
+++ app/src/main/java/com/notableFactory/marvelapp/MainActivity.kt
@@ -2,10 +2,52 @@ package com.notableFactory.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        signIn("imartinez@gmail.com", "Password13")
+
     }
+
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "Clean!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+    private fun registerUser(email:String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Clean register!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+
 }
---
+++ app/src/main/java/com/notableFactory/marvelapp/signIn/utils/SessionManager.kt
@@ -0,0 +1,12 @@
+package com.notableFactory.marvelapp.signIn.utils
+
+class SessionManager {
+
+    fun signInUser(email:String, password:String) {
+
+    }
+
+    fun registerUser(email:String, password:String) {
+
+    }
+}
