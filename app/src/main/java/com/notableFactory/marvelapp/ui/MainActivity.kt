52ea86bb081a4362b71a27a638e46435fabfbf9f
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-11-20 19:04:03 -0300

    search super heroe browse Fragment

--- app/src/main/java/com/notableFactory/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/notableFactory/marvelapp/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.notableFactory.marvelapp.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.notableFactory.marvelapp.R
+import com.notableFactory.marvelapp.ui.home.HomeActivity
 import com.notableFactory.marvelapp.ui.login.LoginFragment
 import com.notableFactory.marvelapp.ui.login.RegisterFragment
 import com.notableFactory.marvelapp.utils.Ui.closeKeyboard
